f094546583ebc92649a8818f73d50242cdb0180c
yemss <yamasamirr@gmail.com>
2024-06-04 21:25:03 +0300

    recoding the website and fixing the structure (navigation bar)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.use('/static', express.static('static'))
 
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Webserver listening on port ${port}`)
+  console.log(`Webserver listening on port localhost:${port}`)
 })
