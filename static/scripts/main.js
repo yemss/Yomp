4d1b990dbe36b42ef82e3da747bf8230cc47fc99
yemss <yamasamirr@gmail.com>
2024-06-10 14:59:45 +0300

    Fixing and making the footer and the article container (character-preview) more responsive

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -3,13 +3,11 @@ const skizzy_btn = document.querySelector("#skizzy-btn");
 const daemon_btn = document.querySelector("#daemon-btn");
 
 const div_parent = document.querySelector(".character-description");
-const p_child = document.createElement("p");
-const h3_element = document.querySelector(".character-name");
+const div_parent2 = document.querySelector(".character-img");
+var p_child = document.createElement("p");
+var h3_element = document.querySelector(".character-name");
+var img = document.createElement("img");
 
-function createDescriptionForCharacter(description) {
-    p_child.textContent = description;
-    div_parent.appendChild(p_child);
-}
 
 function applyStyles(element, styles) {
     for (const property in styles) {
@@ -17,30 +15,52 @@ function applyStyles(element, styles) {
     }
   }
 
+function createDescriptionForCharacter(description) {
+    h3_element.innerHTML = "";
+    p_child.innerHTML = "";
+    p_child.textContent = description;
+    div_parent.appendChild(p_child);
+}
+
+function createImageForCharacter(str) {
+    img.src = str;
+    img.alt = "";
+    div_parent2.appendChild(img);
+    applyStyles(img, {"min-height": "100%", width: "100%"});
+}
+
+createImageForCharacter("static/images/template.png");
+let description = "Wriesly is an 8-bit console who resides in the Yomp world, the world of minigames, with a duty to perserve it from the eventual ruin.";
+createDescriptionForCharacter(description);
+h3_element.textContent = "Wriesly";
+applyStyles(wriesly_btn, {border: "3px solid #93afed"});
+
 wriesly_btn.addEventListener('click', () => {
-    h3_element.textContent = "Wriesly";
-    description = "Wriesly is an 8-bit console who resides in the Yomp world, the world of minigames, with a duty to perserve it from the eventual ruin.";
+    createImageForCharacter("static/images/template.png");
+    let description = "Wriesly is an 8-bit console who resides in the Yomp world, the world of minigames, with a duty to perserve it from the eventual ruin.";
     createDescriptionForCharacter(description);
+    h3_element.textContent = "Wriesly";
     applyStyles(wriesly_btn, {border: "3px solid #93afed"}); //#e36666
     applyStyles(skizzy_btn, {"border-width": 0});
     applyStyles(daemon_btn, {"border-width": 0});
     });
 
 skizzy_btn.addEventListener('click', () => {
-    h3_element.textContent = "Skizzy";
-    description = "Skizzy is a little girl who loves sour candy and bunnies, venturing the vast horizons of Yomp.";
+    createImageForCharacter("static/images/template.png");
+    let description = "Skizzy is a little girl who loves sour candy and bunnies, venturing the vast horizons of Yomp.";
     createDescriptionForCharacter(description);
+    h3_element.textContent = "Skizzy";
     applyStyles(skizzy_btn, {border: "3px solid #93afed"});
     applyStyles(wriesly_btn, {"border-width": 0});
     applyStyles(daemon_btn, {"border-width": 0});
 });
 
 daemon_btn.addEventListener('click', () => {
-    h3_element.textContent = "Daemon";
-    description = "Daemon, the last ancestor of the demon kin, mistakenly arrived within the Yomp realm. The first impression on him seems fearesome but in truth is a considerate child.";
+    createImageForCharacter("static/images/template.png");
+    let description = "Daemon, the last ancestor of the demon kin, mistakenly arrived within the Yomp realm. The first impression on him seems fearesome but in truth is a considerate child.";
     createDescriptionForCharacter(description);
+    h3_element.textContent = "Daemon";
     applyStyles(daemon_btn, {border: "3px solid #93afed"});
     applyStyles(wriesly_btn, {"border-width": 0});
     applyStyles(skizzy_btn, {"border-width": 0});
 });
-
